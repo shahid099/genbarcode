9d05ecaf21589b138523d23acbc1b97991634e46
Muhammad Shahid <108601827+shahid099@users.noreply.github.com>
2024-05-09 07:37:49 +0500

    Background Changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import './App.css';
 const App = () => {
   return (
-    <div className='w-full min-h-screen mainPagebg'>
+    <div className='w-full min-h-screen bg-gray-400'>
         <Navbar />
         <Mainpage />
         <Footer />
